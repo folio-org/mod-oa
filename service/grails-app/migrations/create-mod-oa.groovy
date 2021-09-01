a7187c0844e0bb7f00df8062236fd3a4623b0ce5
Ian Ibbotson <ianibbo@gmail.com>
2021-09-01 11:16:02 +0100

    Added Housekeeping service, added app_setting to migrations

--- service/grails-app/migrations/create-mod-oa.groovy
+++ service/grails-app/migrations/create-mod-oa.groovy
@@ -45,4 +45,23 @@ databaseChangeLog = {
     }
   }
 
+  changeSet(author: "ianibbo (manual)", id: "i202109011115-001") {
+    createTable(tableName: "app_setting") {
+      column(name: "st_id", type: "VARCHAR(36)") {
+        constraints(nullable: "false")
+      }
+      column(name: "st_version", type: "BIGINT") {
+        constraints(nullable: "false")
+      }
+      column(name: 'st_section', type: "VARCHAR(255)")
+      column(name: 'st_key', type: "VARCHAR(255)")
+      column(name: 'st_setting_type', type: "VARCHAR(255)")
+      column(name: 'st_vocab', type: "VARCHAR(255)")
+      column(name: 'st_default_value', type: "VARCHAR(255)")
+      column(name: 'st_value', type: "VARCHAR(255)")
+    }
+
+    addPrimaryKey(columnNames: "st_id", constraintName: "app_setting_PK", tableName: "app_setting")
+  }
+
 }
