ec850fc4145fe52d900cb5ece50830c0a3736520
Jack-Golding <94838251+Jack-Golding@users.noreply.github.com>
2022-06-15 08:48:34 +0100

    fix: Migration fix (#105)
    Reverted changed to previous migration and dropped columns correctly

--- service/grails-app/migrations/create-mod-oa.groovy
+++ service/grails-app/migrations/create-mod-oa.groovy
@@ -810,8 +810,10 @@ databaseChangeLog = {
 
   changeSet(author: "ianibbo (manual)", id: "2022-02-22-1438-001") {
     addColumn(tableName: "charge") {
+      column(name: "ch_payer_fk", type: "VARCHAR(36)")
       column(name: "ch_discount_note", type: "TEXT")
       column(name: "ch_tax", type: "NUMBER(20,10)")
+      column(name: "ch_payer_note", type: "TEXT")
       column(name: "ch_charge_status_fk", type: "VARCHAR(36)")
     }
   }
@@ -1075,5 +1077,10 @@ databaseChangeLog = {
       column(name: "cpy_payer_note", type: "TEXT") { constraints(nullable: "true") }
       column(name: "cpy_owner_fk", type: "VARCHAR(36)") { constraints(nullable: "false") }
     }
+  }
+
+  changeSet(author: "Jack-Golding (manual)", id: "2022-06-14-1659-001") {
+      dropColumn(columnName: "ch_payer_note", tableName: "charge")
+      dropColumn(columnName: "ch_payer_fk", tableName: "charge")
   }
 }
