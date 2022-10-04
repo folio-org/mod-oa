18335849dcc2f79bad6d4bc63d02d721c790ba26
Ian Ibbotson <ianibbo@gmail.com>
2022-10-04 20:47:11 +0100

    Update toolkit dependencies, add missing migrations for toolkit classes

--- service/grails-app/migrations/module-tenant-changelog.groovy
+++ service/grails-app/migrations/module-tenant-changelog.groovy
@@ -3,4 +3,6 @@ databaseChangeLog = {
   include file: 'create-mod-oa.groovy'
   include file: 'create-workflow.groovy'
   include file: 'setup-oa-workflow.groovy'
+  include file: 'wtk/multi-value-custprops.feat.groovy'
+  include file: 'wtk/hidden-appsetting.feat.groovy'
 }
