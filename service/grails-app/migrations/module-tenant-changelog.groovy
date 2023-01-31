a561448247e2e11c069b5c4fa127bc3e919a2003
Jack-Golding <94838251+Jack-Golding@users.noreply.github.com>
2023-01-31 16:24:08 +0000

    fix: Main email data type (#152)
    * fix: Main email data type
    
    Changed p_main_email datatype from VARCHAR(36) to VARCHAR(255)
    
    MODOA-44
    
    * docs: Updated NEWS
    
    Added changes to NEWS

--- service/grails-app/migrations/module-tenant-changelog.groovy
+++ service/grails-app/migrations/module-tenant-changelog.groovy
@@ -2,7 +2,8 @@ databaseChangeLog = {
   include file: 'initial-customisations.groovy'
   include file: 'create-mod-oa.groovy'
   include file: 'create-workflow.groovy'
-  include file: 'setup-oa-workflow.groovy'  
+  include file: 'setup-oa-workflow.groovy'
+  include file: 'update-mod-oa-1-1.groovy'  
   
   // Toolkit features opted into.
   include file: 'wtk/hidden-appsetting.feat.groovy'
