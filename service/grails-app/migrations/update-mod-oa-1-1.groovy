0521de2f72dc872202a6fdfbd3944fd76fa67b3d
Jack-Golding <94838251+Jack-Golding@users.noreply.github.com>
2023-02-21 16:32:36 +0000

    fix: Publication title datatype (#155)
    Modified pr_title datatype from VARCHAR(255) to VARCHAR(4096) to accommodate longer publication titles
    
    MODOA-46

--- service/grails-app/migrations/update-mod-oa-1-1.groovy
+++ service/grails-app/migrations/update-mod-oa-1-1.groovy
@@ -7,4 +7,13 @@ databaseChangeLog = {
       confirm: "Successfully updated the p_main_email column."
     )
   }
+
+  changeSet(author: "Jack-Golding (manual)", id: "2023-02-21-1618-001") {
+    modifyDataType( 
+      tableName: "publication_request",
+      columnName: "pr_title",
+      newDataType: "VARCHAR(4096)",
+      confirm: "Successfully updated the pr_title column."
+    )
+  }
 }
