401b47fc62cc193866b7486280bb44fba618e04a
Jack-Golding <94838251+Jack-Golding@users.noreply.github.com>
2023-07-13 11:55:04 +0100

    refactor: Migration file change (#166)
    Moved migrations for 2.0 from the 1-1 migration file to 2-0 file for consistency

--- service/grails-app/migrations/module-tenant-changelog.groovy
+++ service/grails-app/migrations/module-tenant-changelog.groovy
@@ -3,7 +3,8 @@ databaseChangeLog = {
   include file: 'create-mod-oa.groovy'
   include file: 'create-workflow.groovy'
   include file: 'setup-oa-workflow.groovy'
-  include file: 'update-mod-oa-1-1.groovy'  
+  include file: 'update-mod-oa-1-1.groovy'
+  include file: 'update-mod-oa-2-0.groovy'  
   
   // Toolkit features opted into.
   include file: 'wtk/hidden-appsetting.feat.groovy'
